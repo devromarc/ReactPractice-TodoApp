6073ada134c9a96d02f4527a579a4aea757abd02
devromarc <69194960+devromarc@users.noreply.github.com>
2025-03-31 17:36:14 +0800

    Delete modal completed

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useRef } from "react";
+import Modal from "./utils/Modal";
 
 const SelectedProject = ({ project, onDeleteProject }) => {
   const { title, date, description, id } = project;
+  const modal = useRef();
   return (
-    <div className="w-[35rem] mt-16">
-      <header className="pb-4 mb-4 border-b-2 border-stone-300">
-        <div className="flex items-center justify-between">
-          <h1 className="text-3xl font-bold text-stone-600 mb-2">{title}</h1>
+    <>
+      <Modal ref={modal} buttonCaption="Close" deleteModal>
+        <h2 className="text-xl font-bold text-stone-700 my-4">
+          Are you Sure you want to delete '{title}'?
+        </h2>
+        <div className="flex justify-end gap-2">
           <button
-            onClick={() => {
-              onDeleteProject(id);
-            }}
-            className="text-stone-600 hover:text-stone-950"
+            className="flex-1 px-4 py-2 text-xs md:text-base rounded-md bg-red-700 text-white hover:bg-red-600"
+            onClick={() => onDeleteProject(id)}
           >
-            Delete
+            Yes!
+          </button>
+          <button
+            className="flex-1 px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
+            onClick={() => modal.current.close()}
+          >
+            No
           </button>
         </div>
-        <p className="mb-4 text-stone-400">{description} </p>
-        <p className="text-stone-600 whitespace-pre-wrap">{date}</p>
-      </header>
-      <div></div>
-    </div>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <header className="pb-4 mb-4 border-b-2 border-stone-300">
+          <div className="flex items-center justify-between">
+            <h1 className="text-3xl font-bold text-stone-600 mb-2">{title}</h1>
+            <button
+              onClick={() => modal.current.open()}
+              className="text-stone-600 hover:text-stone-950"
+            >
+              Delete
+            </button>
+          </div>
+          <p className="mb-4 text-stone-400">{description} </p>
+          <p className="text-stone-600 whitespace-pre-wrap">{date}</p>
+        </header>
+        <div></div>
+      </div>
+    </>
   );
 };
 
--- src/components/utils/Modal.jsx
+++ src/components/utils/Modal.jsx
@@ -1,26 +1,31 @@
 import React, { useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children, ref, buttonCaption }) => {
+const Modal = ({ children, ref, buttonCaption, deleteModal }) => {
   const dialog = useRef();
   useImperativeHandle(ref, () => {
     return {
       open() {
         dialog.current.showModal();
       },
+      close() {
+        dialog.current.close();
+      },
     };
   });
   return createPortal(
     <dialog
       ref={dialog}
-      className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md"
+      className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md w-fit"
     >
       {children}
-      <form className="mt-4 text-right" method="dialog">
-        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
-          {buttonCaption}
-        </button>
-      </form>
+      {!deleteModal && (
+        <form className="mt-4 flex justify-end gap-2" method="dialog">
+          <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+            {buttonCaption}
+          </button>
+        </form>
+      )}
     </dialog>,
     document.querySelector("#modal-root")
   );
